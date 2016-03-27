d7d87b1df46cf787f85c2e2b12a96a73cf83eadb
myrkox <berlad.myr@gmail.com>
2016-03-27 14:49:26 +0300

    added action for choosing game type

--- ember-js/app/components/tic-tac-toe/component.js
+++ ember-js/app/components/tic-tac-toe/component.js
@@ -2,4 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
+  gameInitWith: {
+    CROSS: 1,
+    ZERO: 2
+  },
+
+  gameStartedWith: null,
+
+  actions: {
+
+    startGame(gameType) {
+      this.set('gameStartedWith', gameType);
+    }
+  }
+
+
 });
