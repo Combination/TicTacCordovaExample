8e3bb49d41b3a7fa3916bb79a22a47c274769f70
ReenExe <ipourri@gmail.com>
2016-03-31 20:17:17 +0300

    progress

--- react-js/application.jsx
+++ react-js/application.jsx
@@ -1,14 +1,22 @@
-var Header = React.createClass({
-    render: function () {
-        return (
-            <div className="header">
-                <h2><span><b>Х</b></span><span>р</span><span>е</span><span>с</span><span>т</span><span>и</span><span>к</span><span>и</span><span>-</span><span>н</span><span><b>о</b></span><span>л</span><span>и</span><span>к</span><span>и</span></h2>
-            </div>
-        );
-    }
-});
+        var Header = React.createClass({
+            render: function () {
+                return (
+                    <div className="header">
+                        <h2><span><b>Х</b></span><span>р</span><span>е</span><span>с</span><span>т</span><span>и</span><span>к</span><span>и</span><span>-</span><span>н</span><span><b>о</b></span><span>л</span><span>и</span><span>к</span><span>и</span></h2>
+                    </div>
+                );
+            }
+        });
 
-ReactDOM.render(
-    <Header />,
-    document.getElementById('js-app')
-);
+        var Application = React.createClass({
+            render: function() {
+                return (
+                    <Header />
+                );
+            }
+        });
+
+        ReactDOM.render(
+            <Application />,
+            document.getElementById('js-app')
+        );
