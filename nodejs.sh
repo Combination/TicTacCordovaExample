4010ef4c833ffeea5ecab0d1b64e6452601112d8
ReenExe <ipourri@gmail.com>
2016-04-16 16:43:21 +0300

    copy friend`s config

---
+++ nodejs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y node
+sudo apt-get install -y npm
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo npm install -g n
+sudo npm cache clean -f
+sudo n stable
+npm update
+npm install -g supervisor
