71aacc453829a52abcf999807f440776d1835ba8
Golars <titorens@gmail.com>
2016-04-16 16:20:30 +0300

    use in front-end

--- core/game.js
+++ core/game.js
@@ -1,7 +1,7 @@
 var TicTacToe = {};
 
 TicTacToe.Game = function () {
-    
+
 };
 
 TicTacToe.Answer = function () {
@@ -9,7 +9,10 @@ TicTacToe.Answer = function () {
 };
 
 TicTacToe.Game.prototype.getAnswer = function () {
-  return new TicTacToe.Answer();  
+    return new TicTacToe.Answer();
 };
+
+// TODO: need better
+var module = module || {};
 
 module.exports = TicTacToe;
