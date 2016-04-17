9df4a797d42f306b1295ecd2bd7ba5b4c7a62121
ReenExe <ipourri@gmail.com>
2016-04-18 01:27:21 +0300

    progress

--- core/game.js
+++ core/game.js
@@ -1,4 +1,31 @@
-const TicTacToe = {};
+const TicTacToe = {
+    winner: {
+        map: [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+
+            [0, 4, 8],
+            [2, 4, 6]
+        ],
+        is: function (matrix, choose) {
+            let map = TicTacToe.winner.map;
+            for (let i = 0, length = map.length; i < length; ++i) {
+                let line = map[i];
+
+                if (matrix[line[0]] === choose && matrix[line[1]] === choose && matrix[line[2]] === choose) {
+                   return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+};
 
 TicTacToe.Choose = function () {};
 
@@ -8,15 +35,20 @@ TicTacToe.Choose.ZERO = new TicTacToe.Choose('o');
 TicTacToe.Game = function (player, partner) {
     this.player = player;
     this.partner = partner;
-    this.behavior = new TicTacToe.Behavior();
     this.matrix = [];
+    this.behavior = new TicTacToe.Behavior(this);
 
     if (partner.instance === TicTacToe.Choose.CROSS) {
         this.matrix[this.behavior.getFirstPoint()] = partner;
     }
 };
 
-TicTacToe.Behavior = function () {};
+TicTacToe.Behavior = function (game) {
+    /**
+     * @type {TicTacToe.Game}
+     */
+    this.game = game;
+};
 
 TicTacToe.Behavior.prototype.priority = [
     8, 7, 6, 5, 4, 3, 2, 1, 0
@@ -24,10 +56,6 @@ TicTacToe.Behavior.prototype.priority = [
 
 TicTacToe.Behavior.prototype.getFirstPoint = function () {
     return this.priority[0];
-};
-
-TicTacToe.getAnswer = function (player, partner, behavior) {
-
 };
 
 TicTacToe.Game.prototype.getMatrix = function () {
--- tests/tic-tac-toe.js
+++ tests/tic-tac-toe.js
@@ -3,9 +3,7 @@ var TicTacToe = require('../core/game');
 
 describe('Test multi structure', function () {
     it('game', function (done) {
-        var game = new TicTacToe.Game();
-
-        expect(game).to.be.a('object');
+        expect(TicTacToe).to.be.a('object');
 
         done();
     });
