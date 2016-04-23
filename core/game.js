6b3fe48982388b030ddedff679400c6c8f394743
ReenExe <ipourri@gmail.com>
2016-04-23 17:12:54 +0300

    截拳道

--- core/game.js
+++ core/game.js
@@ -105,8 +105,54 @@ TicTacToe.Behavior.prototype.getAnswer = function() {
 TicTacToe.AdvanceBehavior = TicTacToe.Behavior;
 TicTacToe.AdvanceBehavior.prototype = new TicTacToe.Behavior();
 TicTacToe.AdvanceBehavior.prototype.priority = [
-    4, 3, 2, 1, 0, 8, 7, 6, 5
+    4, 6, 2, 0, 1, 3, 5, 7, 8
 ];
+
+TicTacToe.AdvanceBehavior.prototype.getAnswer = function() {
+    var values = this.game.matrix.values;
+    var queue = [];
+
+    for (var i = 0; i < TicTacToe.length; ++i) {
+        var point = this.priority[i];
+
+        if (values[point]) continue;
+
+        queue.push(point);
+    }
+
+    for (var i = 0; i < queue.length; ++i) {
+        var point = queue[i];
+
+        var copy = values.slice();
+
+        copy[point] = this.game.partner;
+
+        var winner = TicTacToe.winner.get(copy, this.game.partner);
+
+        if (winner) {
+            this.game.matrix.set(point, this.game.partner);
+            return new TicTacToe.Answer(winner);
+        }
+    }
+
+    for (var i = 0; i < queue.length; ++i) {
+        var point = queue[i];
+
+        var copy = values.slice();
+
+        copy[point] = this.game.player;
+
+        var winner = TicTacToe.winner.get(copy, this.game.player);
+
+        if (winner) {
+            this.game.matrix.set(point, this.game.partner);
+            return new TicTacToe.Answer(null);
+        }
+    }
+
+    this.game.matrix.set(queue[0], this.game.partner);
+    return new TicTacToe.Answer(null);
+};
 
 TicTacToe.Answer = function (winner) {
     this.winner = winner;
