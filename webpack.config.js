f7e013a98e4c42777a488474ea12f7f88b571692
ReenExe <ipourri@gmail.com>
2016-05-24 20:08:53 +0300

    progress

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        app: './react-js/app'
+    },
+
+    output: {
+        path: __dirname + '/react-js/js/public',
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                include: __dirname + '/react-js',
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
