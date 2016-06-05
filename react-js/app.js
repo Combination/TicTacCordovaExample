ba2cfd6447c2ddfd4987a9d68d5618f8d272fd7b
ReenExe <ipourri@gmail.com>
2016-06-05 16:50:24 +0300

    try `Perf`

--- react-js/app.js
+++ react-js/app.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import Application from 'reen/components/application';
 
+import Perf from 'react-addons-perf';
+
+global.Perf = Perf;
+
 render(
     <Application />,
     document.getElementById('js-app')
