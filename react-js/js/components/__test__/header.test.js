bdf0425ba8bd2a4eb70f7ebdb6bd0384017cbd9d
ReenExe <ipourri@gmail.com>
2016-12-05 01:14:48 +0200

    try all to start header.test.js (#20)

---
+++ react-js/js/components/__test__/header.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+
+describe('game suite', function () {
+    it('example', function () {
+        expect(1).toBe(1);
+    });
+});
