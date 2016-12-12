4e2053f09d65a6d4a9d17eb37a818de39102f249
ReenExe <ipourri@gmail.com>
2016-12-12 19:06:02 +0200

    covers score

--- react-js/js/components/__test__/header.test.js
+++ react-js/js/components/__test__/header.test.js
@@ -1,12 +1,44 @@
 jest.dontMock('../header');
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render } from 'enzyme';
 import Header from 'reen/components/header';
 
 describe('header suite', function () {
-    it('header', function () {
-        const wrapper = shallow(<Header />);
-        expect(wrapper.find('div.header').length).toBe(1);
+    it('first score', function () {
+        const wrapper = render(
+            <Header
+                score={{
+                    player:0,
+                    partner:0
+                }}
+            />
+        );
+
+        const header = wrapper.find('.header');
+        expect(header.length).toBe(1);
+
+        const result = header.find('.result');
+        expect(header.length).toBe(1);
+
+        expect(header.find('.me').text()).toBe('0');
+        expect(header.find('.me+*').text()).toBe('0');
+    });
+
+    it('game score', function () {
+        const wrapper = render(
+            <Header
+                score={{
+                    player:3,
+                    partner:2
+                }}
+            />
+        );
+
+        const header = wrapper.find('.header');
+        const result = header.find('.result');
+
+        expect(header.find('.me').text()).toBe('3');
+        expect(header.find('.me+*').text()).toBe('2');
     });
 });
