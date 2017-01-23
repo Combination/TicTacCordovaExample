4f08eaa35cbc018fa3467fc5c4bd331972926248
ReenExe <ipourri@gmail.com>
2017-01-23 18:49:28 +0200

    start migrate to tape (#23)

--- core/__test__/game.test.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('./../game');
-
-const Game = require('./../game');
-
-describe('game suite', function () {
-    it('example', function () {
-        console.log(Game);
-    });
-});
--- core/__test__/over.test.js
+++
@@ -1,9 +0,0 @@
-jest.dontMock('./../over');
-
-const Over = require('./../over');
-
-describe('class Over suite', function () {
-    it('constructor', function () {
-        console.log(Over);
-    });
-});
