02c1598992a6070189f0245fd987bbcbd7e784d3
ReenExe <ipourri@gmail.com>
2017-01-23 19:24:45 +0200

    covers Matrix (#23)

--- core/test.js
+++ core/test.js
@@ -3,22 +3,46 @@ import Matrix from 'tic-tac-toe/matrix'
 import SequenceBehavior from 'tic-tac-toe/behavior/sequence'
 import Choose from 'tic-tac-toe/choose'
 
-test('SequenceBehavior', function (t) {
-    t.plan(5);
+test('Matrix', function(t) {
+    t.plan(3);
 
     let matrix = new Matrix();
-    const player = Choose.ZERO;
-    const partner = Choose.CROSS;
 
-    const behavior = new SequenceBehavior(matrix, player, partner);
-
-    t.ok(behavior.getFirstPoint() === 8);
     t.deepEqual(matrix.values, []);
+
+    matrix.set(1, Choose.CROSS);
+    matrix.set(2, Choose.ZERO);
+    matrix.set(7, Choose.CROSS);
+    matrix.set(8, Choose.ZERO);
+    matrix.set(3, Choose.CROSS);
+
+    t.ok(matrix.length === 5)
+
+    t.deepEqual(matrix.values, {
+        1: Choose.CROSS,
+        2: Choose.ZERO,
+        7: Choose.CROSS,
+        8: Choose.ZERO,
+        3: Choose.CROSS
+    });
+});
+
+test('SequenceBehavior', function (t) {
+    t.plan(5)
+
+    let matrix = new Matrix()
+    const player = Choose.ZERO
+    const partner = Choose.CROSS
+
+    const behavior = new SequenceBehavior(matrix, player, partner)
+
+    t.ok(behavior.getFirstPoint() === 8)
+    t.deepEqual(matrix.values, [])
 
     let answer = behavior.getAnswer();
     t.ok(answer.getWinner() === null);
     t.deepEqual(matrix.values, {
         8: partner
-    });
+    })
     t.ok(matrix.length === 1)
 });
