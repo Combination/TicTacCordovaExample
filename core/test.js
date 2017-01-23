625354a3207b7686525105c927bdbf3a4bc8c9d0
ReenExe <ipourri@gmail.com>
2017-01-23 19:17:16 +0200

    first answer (#23)

--- core/test.js
+++ core/test.js
@@ -4,13 +4,21 @@ import SequenceBehavior from 'tic-tac-toe/behavior/sequence'
 import Choose from 'tic-tac-toe/choose'
 
 test('SequenceBehavior', function (t) {
-    t.plan(1);
+    t.plan(5);
 
     let matrix = new Matrix();
-    const player = Choose.CROSS;
-    const partner = Choose.ZERO;
+    const player = Choose.ZERO;
+    const partner = Choose.CROSS;
 
     const behavior = new SequenceBehavior(matrix, player, partner);
 
     t.ok(behavior.getFirstPoint() === 8);
+    t.deepEqual(matrix.values, []);
+
+    let answer = behavior.getAnswer();
+    t.ok(answer.getWinner() === null);
+    t.deepEqual(matrix.values, {
+        8: partner
+    });
+    t.ok(matrix.length === 1)
 });
