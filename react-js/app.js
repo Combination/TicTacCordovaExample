62f98dede04b6f413f64e30f1e9d3f82538a04ef
ReenExe <ipourri@gmail.com>
2017-01-31 19:20:46 +0200

    remove `debug` from example

--- react-js/app.js
+++ react-js/app.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import Application from 'reen/components/application';
 
-import Perf from 'react-addons-perf';
-import { whyDidYouUpdate } from 'why-did-you-update'
+/**
+ * For browser performance
+    import Perf from 'react-addons-perf';
+    import { whyDidYouUpdate } from 'why-did-you-update'
 
-
-if (process.env.NODE_ENV !== 'production') {
     whyDidYouUpdate(React);
     global.Perf = Perf;
-}
+ */
 
 render(
     <Application />,
