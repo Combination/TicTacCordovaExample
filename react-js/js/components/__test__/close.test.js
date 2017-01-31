88c9d48282ec898fed008b1ccaaa33edcb63e928
ReenExe <ipourri@gmail.com>
2017-01-31 20:23:01 +0200

    use simple spy for cover click

--- react-js/js/components/__test__/close.test.js
+++ react-js/js/components/__test__/close.test.js
@@ -1,11 +1,17 @@
 import test from 'tape';
+import spy from 'spy';
 
 import React from 'react';
 import { shallow } from 'enzyme';
 import Close from 'reen/components/close';
 
 test('close suite', function (t) {
-    t.plan(1);
-    const wrapper = shallow(<Close />);
+    t.plan(3);
+    const actionSpy = spy();
+    const wrapper = shallow(<Close action={actionSpy} />);
     t.ok(wrapper.find('a').length === 1);
+
+    wrapper.simulate('click');
+    t.ok(actionSpy.called);
+    t.ok(actionSpy.callCount === 1);
 });
