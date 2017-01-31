51cf17a2a05da368015abbb9019b264e6294ff30
ReenExe <ipourri@gmail.com>
2017-01-31 18:22:31 +0200

    5 minute migrate Jest to Tape (#27)

--- react-js/js/components/__test__/score.test.js
+++ react-js/js/components/__test__/score.test.js
@@ -1,35 +1,36 @@
-jest.dontMock('../score');
+import test from 'tape';
 
 import React from 'react';
 import { render } from 'enzyme';
 import Score from 'reen/components/score';
 
-describe('header suite', function () {
-    it('first score', function () {
-        const wrapper = render(
-            <Score
-                score={{
-                    player:0,
-                    partner:0
-                }}
-            />
-        );
+test('start score', function (t) {
+    t.plan(2);
+    const wrapper = render(
+        <Score
+            score={{
+                player:0,
+                partner:0
+            }}
+        />
+    );
 
-        expect(wrapper.find('.me').text()).toBe('0');
-        expect(wrapper.find('.me+*').text()).toBe('0');
-    });
+    t.ok(wrapper.find('.me').text() === '0');
+    t.ok(wrapper.find('.me+*').text() === '0');
+});
 
-    it('game score', function () {
-        const wrapper = render(
-            <Score
-                score={{
-                    player:3,
-                    partner:2
-                }}
-            />
-        );
+test('play score', function (t) {
+    t.plan(2);
 
-        expect(wrapper.find('.me').text()).toBe('3');
-        expect(wrapper.find('.me+*').text()).toBe('2');
-    });
+    const wrapper = render(
+        <Score
+            score={{
+                player:3,
+                partner:2
+            }}
+        />
+    );
+
+    t.ok(wrapper.find('.me').text() === '3');
+    t.ok(wrapper.find('.me+*').text() === '2');
 });
