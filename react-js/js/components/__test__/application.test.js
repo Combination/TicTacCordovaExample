5e2af7c6416bcd110f4c8d0cd8cbbddf43797ee0
ReenExe <ipourri@gmail.com>
2017-01-31 22:06:28 +0200

    assert all empty cell (#31)

--- react-js/js/components/__test__/application.test.js
+++ react-js/js/components/__test__/application.test.js
@@ -1,14 +1,22 @@
 import test from 'tape';
 
 import React from 'react';
-import { render } from 'enzyme';
+import { mount } from 'enzyme';
+import { jsdom } from 'jsdom';
 import Application from 'reen/components/application';
 import GameFactory from 'tic-tac-toe/game.factory';
 import Behavior from 'tic-tac-toe/behavior/sequence';
 
+global.document = jsdom('');
+global.window = document.defaultView;
+
 test('application suite', function (t) {
-    t.plan(1);
-    const wrapper = render(<Application gameFactory={new GameFactory(Behavior)} />);
+    t.plan(10);
+    const wrapper = mount(<Application gameFactory={new GameFactory(Behavior)} />);
     const cells = wrapper.find('div.content button');
     t.ok(cells.length === 9);
+
+    cells.forEach(function (cell) {
+        t.ok(cell.text() === '');
+    });
 });
